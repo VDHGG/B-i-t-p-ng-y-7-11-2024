285eb9b3c51f5a4b36fd379159ac6a80affd4247
VDHGG <vuduchuy2072004@gmail.com>
2024-11-07 16:29:53 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace SimTge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSimThe = new System.Windows.Forms.DataGridView();
+            this.TaiButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSimThe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSimThe
+            // 
+            this.dgvSimThe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSimThe.Location = new System.Drawing.Point(26, 32);
+            this.dgvSimThe.Name = "dgvSimThe";
+            this.dgvSimThe.RowHeadersWidth = 51;
+            this.dgvSimThe.RowTemplate.Height = 24;
+            this.dgvSimThe.Size = new System.Drawing.Size(913, 207);
+            this.dgvSimThe.TabIndex = 0;
+            // 
+            // TaiButton
+            // 
+            this.TaiButton.Location = new System.Drawing.Point(408, 292);
+            this.TaiButton.Name = "TaiButton";
+            this.TaiButton.Size = new System.Drawing.Size(163, 38);
+            this.TaiButton.TabIndex = 1;
+            this.TaiButton.Text = "tải";
+            this.TaiButton.UseVisualStyleBackColor = true;
+            this.TaiButton.Click += new System.EventHandler(this.TaiButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 450);
+            this.Controls.Add(this.TaiButton);
+            this.Controls.Add(this.dgvSimThe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSimThe)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSimThe;
+        private System.Windows.Forms.Button TaiButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Policy;
+using System.Data.Common;
+
+
+namespace SimTge
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void TaiButton_Click(object sender, EventArgs e)
+        {
+           
+            KetNoi ketNoi = new KetNoi();           
+            DataSet ds = ketNoi.LayDuLieu("SELECT * FROM Sim");                       
+
+            if (ds != null && ds.Tables.Count > 0 )
+            {
+              
+                dgvSimThe.DataSource = ds.Tables[0];                             
+            }
+            else
+            {
+                MessageBox.Show("Không lấy được dữ liệu.");
+            }
+        }
+
+        class KetNoi
+        {
+            string conStr = @"Data Source=LAPTOP-LU5D34UJ\SQLEXPRESS;Initial Catalog=SimThe;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+            SqlConnection conn;
+
+            public KetNoi()
+            {
+                conn = new SqlConnection(conStr);
+            }
+
+            public DataSet LayDuLieu(string truyvan)
+            {
+                try
+                {
+                    DataSet ds = new DataSet();
+                    SqlDataAdapter da = new SqlDataAdapter(truyvan, conn);
+                    da.Fill(ds);
+                    return ds;                  
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+
+            public bool ThucThi(string truyvan)
+            {
+                try
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(truyvan, conn);
+                    int r = cmd.ExecuteNonQuery();
+                    conn.Close();
+                    return r > 0;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+
+
+
